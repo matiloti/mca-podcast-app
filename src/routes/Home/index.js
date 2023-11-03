2f24ced32d555359707d87d860d2fa097ecae93d
ZaderCode <60507817+ZaderCode@users.noreply.github.com>
2023-11-03 01:14:34 +0100

    Add home page

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,11 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { PodcastCard } from "../../components/PodcastCard";
 import './styles.css';
 
 export const Home = () => {
 
+    const [podcastList, setPodcastList] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json')
+        .then(response => {
+            setPodcastList(response.data.feed.entry);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }, []);
+
     return (
-        <div>
-            Home
+        <div className="podcastList">
+            {
+                podcastList.map((podcast, i) => <PodcastCard 
+                    key={podcast.id.attributes['im:id']}
+                    id={podcast.id.attributes['im:id']}
+                    title={podcast['im:name'].label}
+                    author={podcast['im:artist'].label}
+                    icon={podcast['im:image'][podcast['im:image'].length - 1].label}
+                />)
+            }
         </div>
     )
 }
