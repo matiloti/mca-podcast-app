3b13f917bb7f36aff9f5a98f10ae0abf8068ccf0
ZaderCode <60507817+ZaderCode@users.noreply.github.com>
2023-11-03 05:12:20 +0100

    Add episode page

--- src/routes/Episode/index.js
+++ src/routes/Episode/index.js
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { PodcastSummary } from "../../components/PodcastSummary";
+import { fetchHundredMostPopularPodcasts } from "../../services/podcast";
+import { fetchEpisodeFromPodcast } from "../../services/episode";
+import './styles.css';
 
 export const Episode = () => {
+
+    const { podcastId, episodeId } = useParams();
+    const [ podcast, setPodcast ] = useState({});
+    const [ episode, setEpisode ] = useState(
+        {
+            id: null,
+            title: '',
+            date: '',
+            duration: ''
+        }
+    );
+
+    useEffect(() => {
+        Promise.all([
+            fetchEpisodeFromPodcast(podcastId, episodeId), 
+            fetchHundredMostPopularPodcasts()
+        ])
+            .then(values => {
+                let podcastResponse =
+                values[1].data.feed.entry.filter(podcastElement => 
+                    podcastElement.id.attributes['im:id'] == podcastId
+                )[0];
+            
+                setPodcast(podcast => ({
+                    ...podcast,
+                    id: podcastResponse.id.attributes['im:id'],
+                    title: podcastResponse['im:name'].label,
+                    author: podcastResponse['im:artist'].label,
+                    description: podcastResponse['summary'].label,
+                    image: podcastResponse['im:image'][podcastResponse['im:image'].length -1].label,
+                }));
+
+                setEpisode(values[0]);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }, [podcastId, episodeId]);
+
     return (
-        <div>Episode</div>
+        <div className="episode">
+            <div>
+                <PodcastSummary podcast={podcast}/>
+            </div>
+            <div>
+                <div className="episode__details">
+                    <h2>{episode.title}</h2>
+                    <p dangerouslySetInnerHTML={{__html: episode.description}}/>
+                    <audio controls className="">
+                        <source src={episode.url} type="audio/ogg"/>
+                        Your browser does not support the audio element
+                    </audio>
+                </div>
+            </div>
+        </div>
     )
 }
