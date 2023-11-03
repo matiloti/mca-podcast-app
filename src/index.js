7fc8a777102c68f244b4b199035d85d3b8412dc1
ZaderCode <60507817+ZaderCode@users.noreply.github.com>
2023-11-03 07:51:48 +0100

    Removed unused files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { ErrorBoundary } from './components/ErrorBoundary';
@@ -14,9 +13,4 @@ createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </ErrorBoundary>
   </React.StrictMode>
-  );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
